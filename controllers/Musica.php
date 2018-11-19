77effeefb8694af887fcf1a99f7998b806da4080
HugoRIF <42448456+HugoRIF@users.noreply.github.com>
2018-11-19 01:53:25 -0600

    Aplicación del Modelo
    se conecta con el modelo, se valida los campos de entrada y se redirecciona segun  el tipo de usuario

--- controllers/Musica.php
+++ controllers/Musica.php
@@ -2,40 +2,26 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Musica extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	function __construct(){
 		parent::__construct();
 		$this->load->helper('form');
 		$this->load->helper('url');
-		$this->load->model('PruebasBD');
+		$this->load->model('LoginM');
+		$this->load->library('session');
 	}
 
 	public function index()
 	{
 		$this->load->view('musica/header');
 		$this->load->view('musica/principal');
-		
 	}
 	public function login(){
+		#Recuperamos datos del form y los guardamos en un arreglo
 		$data = array(
 				'usuario' => $this->input->post('usuario'),
 				'contra' => $this->input->post('contra')
 			);
+		#Validamos que no esten vacios los campos, se mandan alerts
 		if($data['usuario']==""){
 			echo '<script>alert("Ingresa un usuario");</script>';
 			$this->load->view('musica/header');
@@ -45,10 +31,43 @@ class Musica extends CI_Controller {
 			echo '<script>alert("Ingresa una Contraseña");</script>';
 			$this->load->view('musica/header');
 			$this->load->view('musica/principal');
-		}else{
-
 		}
-		//$this->load->view('musica/header');
-		//$this->load->view('musica/info', $data);
+		#Si no estan vacios...
+		else{
+			#Checamos en el modelo si eciste el usuario 
+			$idTipo_Usuario=$this->LoginM->Checar_Usuario($data);
+			if($idTipo_Usuario == 1){
+				#existe como Administrador
+				#recupero el id del usuario y se guarda la session
+				$idUsuario=$this->LoginM->Regresa_idUsuario($data);
+				$usuario_data = array(
+					'id' => $idUsuario,
+					);
+				$this->session->set_userdata($usuario_data);
+				echo($this->session->userdata('id'));
+				echo("Admin");
+				$this->load->view('musica/header');
+				$this->load->view('musica/info',$data);
+			}
+			elseif($idTipo_Usuario == 2){
+				#existe como Usuario
+				#recupero el id del usuario y se guarda la session
+				$idUsuario=$this->LoginM->Regresa_idUsuario($data);
+				$usuario_data = array(
+					'id' => $idUsuario,
+					);
+				$this->session->set_userdata($usuario_data);
+				echo($this->session->userdata('id'));
+				echo("Usuario");
+				$this->load->view('musica/header');
+				$this->load->view('musica/info',$data);
+			}else{
+				#El usuario no existe
+				echo '<script>alert("Usuario sin registro");</script>';
+				$this->load->view('musica/header');
+				$this->load->view('musica/principal');
+			}
+		}
+		
 	}
 }
